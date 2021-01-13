0946fb9a0009eba8f894f3dd415615087741a9dd
Sagar <77392781+sagr612@users.noreply.github.com>
2021-01-13 20:39:47 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,30 +1,27 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
-
 using namespace cv;
 using namespace std;
-
-Mat rotate(Mat src, double angle){
-    Mat dst;
-    Point2f pt(src.cols/2., src.rows/2.); // It Points to the Center of the Image
+Mat rotate(Mat source, double angle) {
+    Mat destination;
+    Point2f pt(source.cols/2., source.rows/2.); // It Points to the Center of the Image
     Mat r = getRotationMatrix2D(pt, angle, 1.0); // 1 denotes original size
-    warpAffine(src, dst, r, Size(src.cols, src.rows));
-    return dst;
+    warpAffine(source, destination, r, Size(source.cols, source.rows));
+    return destination;
 }
-
-int main(){
+int main() {
     string imgname;
     double ang;
-    Mat src, dst;
+    Mat source, destination;
 
-    cout << "Enter the Path of the Image followed by Extension: ";
+    cout << "Enter the Path of the Image followed by Extension : ";
     cin >> imgname;
     src = imread(imgname, CV_LOAD_IMAGE_UNCHANGED); // Reading the Image
-    cout << "Enter the angle of Rotation: ";
+    cout << "Enter the angle of Rotation : ";
     cin >> ang;
     
-    dst = rotate(src, ang);
+    destination = rotate(source, ang);
 
     // Show the Original Image
     const char* pzOriginalImage = "Original Image";
